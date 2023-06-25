ff591827f3069a99f8d5dd42492e1a42e35d4a8a
KP <kaungpyaeheinmtu@gmail.com>
2023-06-25 14:42:16 +0630

    respnse files done for git methos

---
+++ lib/data/model/get_all_posts_response.dart
@@ -0,0 +1,20 @@
+class GetAllPostsResponse {
+  GetAllPostsResponse({
+      this.id, 
+      this.title,});
+
+  GetAllPostsResponse.fromJson(dynamic json) {
+    id = json['id'];
+    title = json['title'];
+  }
+  num? id;
+  String? title;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['id'] = id;
+    map['title'] = title;
+    return map;
+  }
+
+}
