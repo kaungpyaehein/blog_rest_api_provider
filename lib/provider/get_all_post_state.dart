29ba9d442d1ae6a5350e79fa4c2758ea200bdb9a
KP <kaungpyaeheinmtu@gmail.com>
2023-06-27 14:22:01 +0630

    get all state and provider done

---
+++ lib/provider/get_all_post_state.dart
@@ -0,0 +1,17 @@
+import '../data/model/get_all_posts_response.dart';
+
+abstract class GetAllPostState { }
+
+class GetAllPostLoading extends GetAllPostState {}
+
+class GetAllPostSuccess extends GetAllPostState {
+  final List<GetAllPostsResponse> getAllPostList;
+
+  GetAllPostSuccess(this.getAllPostList);
+}
+
+class GetAllPostFailed extends GetAllPostState {
+  final String errorMessage;
+
+  GetAllPostFailed(this.errorMessage);
+}
---
+++ lib/provider/get_all_provider.dart
@@ -0,0 +1,22 @@
+import 'package:blog_rest_api_provider/data/model/get_all_posts_response.dart';
+import 'package:blog_rest_api_provider/data/service/blog_api_service.dart';
+import 'package:blog_rest_api_provider/provider/get_all_post_state.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+
+class GetAllProvider extends ChangeNotifier {
+  GetAllPostState getAllPostState = GetAllPostLoading();
+  final BlogApiService _apiService = BlogApiService();
+  Future<void> getAllPost() async {
+    getAllPostState = GetAllPostLoading();
+    notifyListeners();
+    try {
+      List<GetAllPostsResponse> getAllPostList =
+          await _apiService.getAllPosts();
+      getAllPostState = GetAllPostSuccess(getAllPostList);
+      notifyListeners();
+    } catch (e) {
+      getAllPostState = GetAllPostFailed(e.toString());
+    }
+  }
+}
